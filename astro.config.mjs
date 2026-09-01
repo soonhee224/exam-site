97eb11b5cf77c2e0af46553281962a2ee65ee8b9
soonhee224 <soonhee224@gmail.com>
2026-09-01 22:19:42 +0900

    feat: 기출문제 사이트 설정2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
+  site: 'https://tongsin.org',
+  output: 'static',
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
