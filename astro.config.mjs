4e631da65f1032fc9977280262c98711cb62b91d
soonhee224 <soonhee224@gmail.com>
2026-09-02 10:29:41 +0900

    feat: add sitemap and robots.txt for Google Search Console

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@ import { defineConfig } from 'astro/config';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://tongsin.org',
   output: 'static',
+
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
   },
+
+  integrations: [sitemap()],
 });
